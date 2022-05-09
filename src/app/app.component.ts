d9bc0dd94c550020336dd1c26e70d1ef59662e25
Akbar Haider <59404010+ahaider004@users.noreply.github.com>
2022-05-09 14:53:05 -0400

    AR-001:
    - created the front end for the website with the photos hard coded in app component.ts.
    
    todo:
    
    - fix image sizes once it switches to mobile view
    - research ways to make a backend so the user can upload pictures from an admin account and remove hardcoded pictures.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  imageSrc: string;
+  imageAlt: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adil.photography.webapp';
+  data: Item[] = [
+    {
+      imageSrc: './assets/images/work1.jpg',
+      imageAlt: '1'
+    },
+    {
+      imageSrc: './assets/images/work2.jpg',
+      imageAlt: '2'
+    },
+    {
+      imageSrc: './assets/images/work3.jpg',
+      imageAlt: '3'
+    },
+    {
+      imageSrc: './assets/images/work4.jpg',
+      imageAlt: '4'
+    },
+    {
+      imageSrc: './assets/images/work6.jpg',
+      imageAlt: '5'
+    },
+    {
+      imageSrc: './assets/images/work7.jpg',
+      imageAlt: '6'
+    },
+    {
+      imageSrc: './assets/images/work8.jpg',
+      imageAlt: '7'
+    },
+    {
+      imageSrc: './assets/images/work10.jpg',
+      imageAlt: '8'
+    },
+    {
+      imageSrc: './assets/images/work12.jpg',
+      imageAlt: '9'
+    },
+    {
+      imageSrc: './assets/images/work13.jpg',
+      imageAlt: '10'
+    },
+    {
+      imageSrc: './assets/images/work14.jpg',
+      imageAlt: '11'
+    },
+    {
+      imageSrc: './assets/images/work15.jpg',
+      imageAlt: '12'
+    },
+    {
+      imageSrc: './assets/images/work17.jpg',
+      imageAlt: '13'
+    }
+  ]
 }
---
+++ src/app/gallery-lightbox/gallery-lightbox.component.ts
@@ -0,0 +1,84 @@
+import { animate, AnimationEvent, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+
+interface Item {
+  imageSrc: string;
+  imageAlt: string;
+}
+
+@Component({
+  selector: 'app-gallery-lightbox',
+  templateUrl: './gallery-lightbox.component.html',
+  styleUrls: ['./gallery-lightbox.component.scss'],
+  animations:[
+    trigger('animation',[
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('150ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('150ms', style({transform: 'scale(0.5)'}))
+      ])
+    ]),
+    trigger('animations2', [
+      transition(':leave',[
+        style({opacity: 1}),
+        animate('50ms',style({opacity: 0.8}))
+      ])
+
+    ])
+
+  ]
+})
+export class GalleryLightboxComponent implements OnInit {
+  @Input() galleryData: Item[] = [];
+  @Input() showCount = false;
+
+  previewImage = false;
+  showMask = false;
+  currentLightBoxImage: Item = this.galleryData[0];
+  currentIndex = 0;
+  controls = true;
+  
+
+  
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  onPreviewImage(index: number):void{
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightBoxImage = this.galleryData[index];
+  }
+
+  onAnimationEnd(event: AnimationEvent){
+    if(event.toState === 'void') {
+      this.showMask = false;
+    }
+  }
+
+  onClosePreview(){
+    this.previewImage = false;
+  }
+
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex>this.galleryData.length -1){
+      this.currentIndex = 0;
+    }
+    this.currentLightBoxImage = this.galleryData[this.currentIndex];
+  }
+
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0) {
+      this.currentIndex = this.galleryData.length - 1;
+    }
+    this.currentLightBoxImage = this.galleryData[this.currentIndex];   
+  }
+
+}
